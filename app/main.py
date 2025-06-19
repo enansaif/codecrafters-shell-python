ffcdc90e579b5dbb360db1994a11c39bc16fbbb2
enansaif <enansaifme33@gmail.com>
2025-06-20 00:20:56 +0600

    #pn5 exit command

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        command_split = command.split()
+        if command_split[0] == 'exit':
+            sys.stdout.write(command_split[1])
+            break
         print(f"{command}: command not found")
 
 
