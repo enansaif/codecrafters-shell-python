7b16798a0be0e28cdaf587f343d82402c76f2bfd
enansaif <enansaifme33@gmail.com>
2025-06-20 00:26:06 +0600

    #pn5 exit command retry1

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def main():
         command = input()
         command_split = command.split()
         if command_split[0] == 'exit':
-            sys.stdout.write(command_split[1])
             break
         print(f"{command}: command not found")
 
