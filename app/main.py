5677c0366e23174e8fb459b4938d01a242cacabd
enansaif <enansaifme33@gmail.com>
2025-06-21 00:30:22 +0600

    #ez5 builtins + code refactor command pattern

--- app/main.py
+++ app/main.py
@@ -1,18 +1,52 @@
 import sys
 
+class Command:
+    def execute(self, arge):
+        raise NotImplementedError("Execute function has to be implemented")
 
-def main():
-    while True:
-        sys.stdout.write("$ ")
-        command = input()
-        command_split = command.split()
-        if command_split[0] == 'exit':
-            break
-        elif command_split[0] == 'echo':
-            print(" ".join(command_split[1:]))
+class EchoCommand(Command):
+    def execute(self, args):
+        print(" ".join(args))
+
+class ExitCommand(Command):
+    def execute(self, args):
+        sys.exit()
+
+class TypeCommand(Command):
+    def execute(self, args):
+        if not args:
+            print("no command given")
+        elif args[0] in ['echo', 'exit', 'type']:
+            print(f"{args[0]} is a shell builtin")
+        else:
+            print(f"{args[0]}: not found")
+
+class Shell:
+    def __init__(self):
+        self.commands = {
+            'echo' : EchoCommand(),
+            'exit' : ExitCommand(),
+            'type' : TypeCommand(),
+        }
+
+    def execute_command(self, command, args):
+        if command in self.commands:
+            target = self.commands[command]
+            target.execute(args)
         else:
             print(f"{command}: command not found")
 
+def main():
+    shell = Shell()
+    while True:
+        try:
+            parts = input("$ ").lower().split()
+            if not parts:
+                continue
+            command, *args = parts
+            shell.execute_command(command, args)
+        except Exception as e:
+            print(f"Error {e}")
 
 if __name__ == "__main__":
     main()
